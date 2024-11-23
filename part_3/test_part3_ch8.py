686c444c5b90c8e7b86bed9b511c2a766d5f215a
Viacheslav <iexploudi@gmail.com>
2024-11-23 23:51:18 +0400

    check fixture level

---
+++ part_3/test_part3_ch8.py
@@ -0,0 +1,25 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+# задаем уровень применимости фикстура “function”, “class”, “module”, “session”
+@pytest.fixture(scope="class")
+def driver():
+	print("\nstart browser...")
+	driver = webdriver.Chrome()
+	yield driver
+	print("\nclose browser...")
+	driver.close()
+
+
+class TestMainPage:
+	def test_guest_should_see_login_link(self, driver):
+		driver.get(link)
+		driver.find_element(By.CSS_SELECTOR, "#login_link")
+
+	def test_guest_should_see_basket_link_on_the_main_page(self, driver):
+		driver.get(link)
+		driver.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
